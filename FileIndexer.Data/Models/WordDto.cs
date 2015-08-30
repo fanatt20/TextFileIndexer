94ab6692aeda3c9c58eddf544972cb69af1d8c43
Venzhyk Andrey <fanatt20@gmail.com>
2015-08-30 10:34:53 +0300

    Delete redundant projects

--- FileIndexer.Data/Models/WordDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FileIndexer.Data.Models
-{
-    public class WordDto
-    {
-        public string Value { get; set; }
-        public int RowPosition { get; set; }
-        public int ColumnPosition { get; set; }
-    }
-}
